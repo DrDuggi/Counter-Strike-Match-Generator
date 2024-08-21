1563fcc98cd33d7bd21b8fe658eb5362e33bf127
DrDuggi <62102560+DrDuggi@users.noreply.github.com>
2024-08-21 12:05:06 +0200

    Ver. 1.0

--- main.gd
+++ main.gd
@@ -9,16 +9,16 @@ var MaxRound = 30 # Done
 var FreezeTime = 10 # Done
 var FreeArmor = 0 # Done
 var Overtime = true # idk
-var BotQuota = 11 # False
+var BotQuota = 5 # False
 var RoundTime = 2 # Done
 var halftime = true # idk
 var GrenadeLimit = 5 # Done
 var BotMimik = false # False
 var Enemy = false # False
+var PlayerCount = 1
+var BotNumber
 # Not game specific
 var musictrack = false # Music for the Program
-
-
 
 
 # Called when the node enters the scene tree for the first time.
@@ -34,9 +34,12 @@ func _process(delta):
 		BotTeamFinish = "T"
 	else:
 		BotTeamFinish = "CT"
+	BotNumber = BotQuota + PlayerCount
 
 
 func _on_close_pressed():
+	$AudioStreamPlayer2D4.play()
+	await get_tree().create_timer(0.1).timeout
 	get_tree().quit()
 
 
@@ -49,6 +52,7 @@ func _on_team_item_selected(index: int) -> void:
 		BotTeam = 1
 	else:
 		BotTeam = 0
+	$AudioStreamPlayer2D2.play()
 
 func _on_difficulty_item_selected(index: int) -> void:
 	match index:
@@ -64,6 +68,7 @@ func _on_difficulty_item_selected(index: int) -> void:
 			BotDifficulty = 4
 		5:
 			BotDifficulty = 5
+	$AudioStreamPlayer2D2.play()
 
 func _on_start_money_item_selected(index: int) -> void:
 	match index:
@@ -89,6 +94,7 @@ func _on_start_money_item_selected(index: int) -> void:
 			StartMoney = 6500
 		10:
 			StartMoney = 12000
+	$AudioStreamPlayer2D2.play()
 
 func _on_max_money_item_selected(index: int) -> void:
 	match index:
@@ -112,6 +118,7 @@ func _on_max_money_item_selected(index: int) -> void:
 			MaxMoney = 25600
 		9:
 			MaxMoney = 51200
+	$AudioStreamPlayer2D2.play()
 
 func _on_freeze_timer_item_selected(index: int) -> void:
 	match index:
@@ -129,6 +136,7 @@ func _on_freeze_timer_item_selected(index: int) -> void:
 			FreezeTime = 25
 		6:
 			FreezeTime = 30
+	$AudioStreamPlayer2D2.play()
 
 func _on_max_rounds_item_selected(index: int) -> void:
 	match index:
@@ -140,6 +148,7 @@ func _on_max_rounds_item_selected(index: int) -> void:
 			MaxRound = 30
 		3:
 			MaxRound = 60
+	$AudioStreamPlayer2D2.play()
 
 func _on_grenade_limit_item_selected(index: int) -> void:
 	match index:
@@ -159,6 +168,7 @@ func _on_grenade_limit_item_selected(index: int) -> void:
 			GrenadeLimit = 6
 		7:
 			GrenadeLimit = 8
+	$AudioStreamPlayer2D2.play()
 
 func _on_round_time_item_selected(index: int) -> void:
 	match index:
@@ -172,33 +182,79 @@ func _on_round_time_item_selected(index: int) -> void:
 			RoundTime = 3
 		4:
 			RoundTime = 5
+	$AudioStreamPlayer2D2.play()
 
 func _on_overtime_toggled(toggled_on: bool) -> void:
 	if toggled_on == true:
 		Overtime = true
 	else:
 		Overtime = false
+	$AudioStreamPlayer2D2.play()
 
 func _on_halftime_toggled(toggled_on: bool) -> void:
 	if toggled_on == true:
 		halftime = true
 	else:
 		halftime = false
+	$AudioStreamPlayer2D2.play()
 
 func _on_free_armor_toggled(toggled_on: bool) -> void:
 	if toggled_on == true:
-		FreeArmor = 1
+		FreeArmor = 2
 	else:
 		FreeArmor = 0
+	$AudioStreamPlayer2D2.play()
+
+func _on_bot_count_item_selected(index: int) -> void:
+	match index:
+		0:
+			BotQuota = 1
+		1:
+			BotQuota = 2
+		2:
+			BotQuota = 3
+		3:
+			BotQuota = 4
+		4:
+			BotQuota = 5
+		5:
+			BotQuota = 6
+		6:
+			BotQuota = 7
+		7:
+			BotQuota = 8
+		8:
+			BotQuota = 9
+		9:
+			BotQuota = 10
+	$AudioStreamPlayer2D2.play()
+
+func _on_player_count_item_selected(index: int) -> void:
+	match index:
+		0:
+			PlayerCount = 1
+		1:
+			PlayerCount = 2
+		2:
+			PlayerCount = 3
+		3:
+			PlayerCount = 4
+		4:
+			PlayerCount = 5
+	$AudioStreamPlayer2D2.play()
 
 
 
 
+func Aftermath():
+	pass
 
 func _on_export_pressed() -> void:
 	$TextEdit.text = "Exporting..."
-	$TextEdit.text = "bot_kick; mp_limitteams %d" % BotQuota + "; mp_autokick 0; mp_autoteambalance 0; bot_join_team " + str(BotTeamFinish) + "; bot_quota_mode fill; bot_quota %d" % BotQuota + "; mp_warmup_end; mp_freezetime %d" % FreezeTime + "; mp_free_armor %d" % FreeArmor + "; mp_roundtime %d" % RoundTime + "; mp_roundtime_defuse %d" % RoundTime + "; mp_startmoney %d" % StartMoney + "; mp_maxmoney %d" % MaxMoney + "; mp_maxrounds %d" % MaxRound +"; mp_halftime " + str(halftime) + "; mp_overtime_enable " + str(Overtime) + "; ammo_grenade_limit_total %d" % GrenadeLimit + "; bot_difficulty %d" % BotDifficulty + "; mp_restartgame 1"
-
-
+	$AnimationPlayer.play("RESET")
+	$AudioStreamPlayer2D3.play()
+	$TextEdit.text = "bot_kick; mp_limitteams %d" % BotNumber + "; mp_autokick 0; mp_autoteambalance 0; bot_join_team " + str(BotTeamFinish) + "; bot_quota_mode fill; bot_quota %d" % BotNumber + "; mp_warmup_end; mp_freezetime %d" % FreezeTime + "; mp_free_armor %d" % FreeArmor + "; mp_roundtime %d" % RoundTime + "; mp_roundtime_defuse %d" % RoundTime + "; mp_startmoney %d" % StartMoney + "; mp_maxmoney %d" % MaxMoney + "; mp_maxrounds %d" % MaxRound +"; mp_halftime " + str(halftime) + "; mp_overtime_enable " + str(Overtime) + "; ammo_grenade_limit_total %d" % GrenadeLimit + "; bot_difficulty %d" % BotDifficulty + "; mp_restartgame 1"
+	DisplayServer.clipboard_set($TextEdit.text)
+	$AnimationPlayer.play("Copied")
 
 # Thanks for checking out my code!
